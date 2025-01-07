04288a32ab588188d7d22a7268b2ead114df6049
Solovev Alexander <solovyov_alex@mail.ru>
2025-01-07 20:57:14 +0500

    ter-hw-2.2

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -30,6 +30,53 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+variable "vm_web_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "Yandex compute image family"
+}
+
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "Yandex compute VM name"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v2"
+  description = "Yandex compute VM platform id"
+}
+
+variable "vm_web_cores" {
+  type        = number
+  default     = 2
+  description = "Yandex compute VM cores"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default     = 1
+  description = "Yandex compute VM memory"
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  default     = 5
+  description = "Yandex compute VM core_fraction"
+}
+
+variable "vm_web_preemptible" {
+  type        = bool
+  default     = true
+  description = "Yandex compute VM preemptible"
+}
+
+variable "vm_web_nat" {
+  type        = bool
+  default     = true
+  description = "Yandex compute VM nat"
+}
 
 ###ssh vars
 
