7a670d15c38b144c868a886e3d96ed4db587b2f0
Solovev Alexander <solovyov_alex@mail.ru>
2025-01-10 09:53:15 +0500

    ter-hw-03.01

--- 03/src/providers.tf
+++ 03/src/providers.tf
@@ -4,11 +4,12 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = "~>1.8.4"
+  required_version = ">=1.8.4"
 }
 
 provider "yandex" {
-  token     = var.token
+  #token     = var.token
+  service_account_key_file = file("~/.authorized_key.json")
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.default_zone
--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -1,16 +1,13 @@
 ###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
-
 variable "cloud_id" {
   type        = string
+  default     = "b1g1li51c4ebgmu7e9s6"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1gbldqcbvmq6hh0agh0"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -19,14 +16,26 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
+# variable "default_zone_b" {
+#   type        = string
+#   default     = "ru-central1-b"
+#   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+# }
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
+# variable "default_cidr_b" {
+#   type        = list(string)
+#   default     = ["10.0.2.0/24"]
+#   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+# }
+
 variable "vpc_name" {
   type        = string
   default     = "develop"
-  description = "VPC network&subnet name"
+  description = "VPC network & subnet name"
 }
